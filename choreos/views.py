dc64124f459cccc055167fddf2f5040b4cf15281
yunchipang <yunchipang@gmail.com>
2023-02-20 20:07:05 -0800

    refactor views with generic class-based views

--- choreos/views.py
+++ choreos/views.py
@@ -1,45 +1,18 @@
 from choreos.models import Choreography
 from choreos.serializers import ChoreographySerializer
-
-from rest_framework import status
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class ChoreographyList(mixins.ListModelMixin,
-                       mixins.CreateModelMixin, 
-                       generics.GenericAPIView):
+class ChoreographyList(generics.ListCreateAPIView):
     """
     List all choreographies, or create a new choreography.
     """
     queryset = Choreography.objects.all()
     serializer_class = ChoreographySerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class ChoreographyDetail(mixins.RetrieveModelMixin,
-                         mixins.UpdateModelMixin,
-                         mixins.DestroyModelMixin,
-                         generics.GenericAPIView):
+class ChoreographyDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a choreography.
     """
     queryset = Choreography.objects.all()
     serializer_class = ChoreographySerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
