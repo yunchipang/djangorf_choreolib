ec107605f31cec165bb1ab3eece80381ec9870b0
yunchipang <yunchipang@gmail.com>
2023-02-20 20:05:28 -0800

    refactor views using mixins

--- choreos/views.py
+++ choreos/views.py
@@ -6,47 +6,40 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import mixins
+from rest_framework import generics
 
-class ChoreographyList(APIView):
+
+class ChoreographyList(mixins.ListModelMixin,
+                       mixins.CreateModelMixin, 
+                       generics.GenericAPIView):
     """
     List all choreographies, or create a new choreography.
     """
-    def get(self, request, format=None):
-        choreographies = Choreography.objects.all()
-        serializer = ChoreographySerializer(choreographies, many=True)
-        return Response(serializer.data)
+    queryset = Choreography.objects.all()
+    serializer_class = ChoreographySerializer
 
-    def post(self, request, format=None):
-        serializer = ChoreographySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class ChoreographyDetail(APIView):
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class ChoreographyDetail(mixins.RetrieveModelMixin,
+                         mixins.UpdateModelMixin,
+                         mixins.DestroyModelMixin,
+                         generics.GenericAPIView):
     """
     Retrieve, update or delete a choreography.
     """
-    def get_object(self, pk):
-        try:
-            return Choreography.objects.get(pk=pk)
-        except Choreography.DoesNotExist:
-            raise Http404
+    queryset = Choreography.objects.all()
+    serializer_class = ChoreographySerializer
 
-    def get(self, request, pk, format=None):
-        choreography = self.get_object(pk)
-        serializer = ChoreographySerializer(choreography)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def post(self, request, pk, format=None):
-        choreography = self.get_object(pk)
-        serializer = ChoreographySerializer(choreography, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        choreography = self.get_object(pk)
-        choreography.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
